d54440bbb66aea0f54e9ff1ff43fa1337678ec32
Shuvanshu Kishor <Shuvanshu126@gmail.com>
2019-07-29 17:19:40 +0530

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Data.swift
+//  Todoey
+//
+//  Created by Shuvanshu Kishor on 19/07/19.
+//  Copyright © 2019 Shuvanshu Kishor. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
